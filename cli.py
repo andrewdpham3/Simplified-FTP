3187c06768d606d4b59b8b55f07047e487c53c7d
Mundo Guzman <mundo_guzman@yahoo.com>
2018-12-22 22:41:52 -0800

    Update cli.py
    I think I need an interface from libftp to list files from the server.

--- cli.py
+++ cli.py
@@ -24,38 +24,56 @@ done = False
 
 control.connect((args.address, int(args.port)))
 
+
+################# Command Definitions ######################
+
 def get(filename):
-	print("Getting file:", filename)
+	""" Downloads file from the server """
+	print('Downloading file:', filename)
 	port = random.randint(6000,7000)
 	message = bytes(("g;" + filename + ";" + str(port) + "\r\n"), "utf-8")
 	control.sendall(message)
 	s = listener_open(port)
-	receive_file(filename, s)
+	receive_file(filename,s)
 
-#TODO: put
 
-#TODO: ls
+def put(filename):
+	""" Uploads file to the server """
+	print('Uploading file:', filename)
+	port = random.randint(6000,7000)
+	s = sender_open(port)
+	send_file(filename,s)
+
+
+def ls(directory):
+	""" List files on the server """ 
+	print('Listing files on server...')
+	port = random.randint(6000,7000)
+	s = listener_open(port)
+	ls_files(directory,s)
+
 
 try:
 	while not done:
 		command = input("ftp> ").split()
+		command = command.upper()
 
-		if command[0] == "get":
+		if command[0] == 'GET':
 			get(command[1])
 
-		elif command[0] == "put":
+		elif command[0] == 'PUT':
 			put(command[1])
 
-		elif command[0] == "ls":
+		elif command[0] == 'LS':
 			ls(command[1])
 
-		elif command[0] == "quit":
+		elif command[0] == 'QUIT':
 			control.close()
 			done = True
 
 		else:
 			print()
-			print("Unknown command entered!")
+			print('The command "{0}" is not recognized ',command[0])
 			print("Please use one of the following commands:")
 			print("get <filename>")
 			print("put <filename>")
