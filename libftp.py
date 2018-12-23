3e14575bfcf676190ef49404195debafc431e5f3
Coleman Nugent <colemannugent@gmail.com>
2018-12-22 16:05:40 -0800

    libftp.py: Add common functions

--- libftp.py
+++ libftp.py
@@ -1,3 +1,6 @@
+import socket
+import sys
+
 # Read data from the control channel and return a tuple of arguments
 def control_get(conn):
 	partial = ""
@@ -19,3 +22,49 @@ def control_get(conn):
 			break
 
 	return partial.split(";")
+
+# Open and return a data channel connection
+def sender_open(addr, port):
+	try:
+		print("addr:", addr)
+		with socket.create_connection((str(addr), port)) as s:
+			return s
+	except IOError as e:
+		print("IOError while opening sender channel: ", e)
+		sys.exit(1)
+
+# Open and return a data channel connection and address object
+def listener_open(port):
+	try:
+		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+			s.bind(('', port))
+			s.listen()
+			conn, addr = s.accept()
+			return (conn, addr)
+	except IOError as e:
+		print("IOError while opening listener channel: ", e)
+		sys.exit(1)
+
+# Send a file over a socket
+def send_file(filename, sock):
+	with open(filename, 'rb') as f:
+		print("Sending file", filename, "...")
+		
+		block = f.read(1024)
+		while (block):
+			sock.send(block)
+			block = f.read(1024)
+		f.close()
+		sock.close()
+
+# Receive a file over a socket
+def receive_file(filename, sock):
+	with open(filename, 'wb') as f:
+		print("Receiving file", filename, "...")
+		
+		block = sock.recv(1024)
+		while (block):
+			f.write(block)
+			block = sock.recv(1024)
+		f.close()
+		sock.close()
